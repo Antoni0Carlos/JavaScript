4d7c567aea056f89d181795581ea30a3d823c821
Antônio carlos <antonicar435@gmail.com>
2021-12-15 10:37:04 -0300

    Adicionar C

--- API/tempo.js
+++ API/tempo.js
@@ -1,7 +1,14 @@
 //ESTUDOS DE APIs EM  JS 
 
-function tempo(request, response) {
+async function tempo(request, response) {
+    const apiSecret = process.env.CONVERTKIT_API_SECRET;
     const dynamicDate = new Date();
+
+    const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers`)
+    const subscribersResponseJson = await subscribersResponse.json();
+    const inscritos = subscribersResponseJson.total_subscribers;
+
+    response.setHeader('Cache-Control', 's-maxage=10, stale-whilee-revalidate');
 
     response.json({
         date: dynamicDate.toGMTString()
