cbe7e7e69327c1ea194b497afb8676532e7849c4
Antônio carlos <antonicar435@gmail.com>
2022-01-10 14:45:41 -0300

    parte 2

--- Heranca/heranca.js
+++ Heranca/heranca.js
@@ -1,53 +1,35 @@
 class Dev{
-    constructor(nome, idade, linguagem){
+    constructor(nome, idade, tipoDeDev, linguagem){
         this.nome = nome;
         this.idade = idade;
+        this.tipoDeDev = tipoDeDev;
         this.linguagem = linguagem;
     }
     
     saudacao() {
-        console.log(`Oi! Sou Dev Frontend, trabalho com ${this.linguagem} e me chamo ${this.nome} \n\n`);
+        console.log(`Oi! Sou Dev ${this.tipoDeDev}, trabalho com ${this.linguagem} e me chamo ${this.nome} \n\n`);
     }
 
 }
 
-/*
-const dev = new Dev ("Carlos", 25, "Javascript");
-console.log(dev);
-dev.saudacao();*/
-
 class Frontend extends Dev{
-    constructor(nome, idade, linguagem, framework){
-        super();
-        this.nome = nome;
-        this.idade = idade;
-        this.linguagem = linguagem;
+    constructor(nome, idade, linguagem, tipoDeDev, framework){
+        super(nome, idade, linguagem, tipoDeDev);
         this.framework = framework;
-    }
-    
-    saudacao() {
-        console.log(`Oi! Sou Dev Frontend, trabalho com ${this.linguagem} e me chamo ${this.nome} \n\n`);
     }
 
 }
 
 class Backend extends Dev {
-    constructor(nome, idade, linguagem, bancodedados){
-        super();
-        this.nome = nome;
-        this.idade = idade;
-        this.linguagem = linguagem;
+    constructor(nome, idade, linguagem, tipoDeDev,  bancodedados){
+        super(nome, idade, linguagem, tipoDeDev);
         this.bancodedados = bancodedados;
-    }
-    
-    saudacao() {
-        console.log(`Oi! Sou Dev Backend, trabalho com ${this.linguagem} e me chamo ${this.nome} \n\n`);
     }
 
 }
 
-const frontend1 = new Frontend("Antônio", "35", "Javascript", "React");
-const backend2 = new Backend("Carlos", "22", "Java", "SQL Server");
+const frontend1 = new Frontend("Antônio", "35", "Frontend", "Javascript", "React");
+const backend2 = new Backend("Carlos", "22",  "Backend", "Java", "SQL Server");
 
 console.log(frontend1);
 frontend1.saudacao();
