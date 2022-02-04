f326a8a184dd1463165dd4b867a642eeae1b676f
Antônio carlos <antonicar435@gmail.com>
2022-02-03 21:01:51 -0300

    docs: estudo

---
+++ destructirng/script.js
@@ -0,0 +1,15 @@
+/*let idade;
+
+const pessoa = {
+    name: "Elton", 
+    age: "26"
+};
+
+({ age: idade}  =  pessoa)
+
+console.log(idade)*/
+
+const frutas = ['banana', 'pera', 'uva', 'melão']
+
+const [banana, ...rest] = frutas
+console.log(frutas)
