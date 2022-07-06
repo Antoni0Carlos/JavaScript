611e842abaeee1fb2d68cd81dad19cf5801d7d44
carlos <antonicar345@gmail.com>
2022-07-06 09:43:47 -0300

    Estudo_de_regExp.js

---
+++ Expressoes_regulares/regExp copy.js
@@ -0,0 +1,10 @@
+const { text } = require('./regTest');
+const regExp1 = /joão/gi; 
+/*
+console.log(text);
+console.log(text.match(regExp1));
+console.log(text.replace(/João/gi, 'Felipe'));
+console.log(text.replace(/(João|Maria)/gi, function(input) {
+    return '############ ' + input.toUpperCase() + '############# ';
+}));
+*/
